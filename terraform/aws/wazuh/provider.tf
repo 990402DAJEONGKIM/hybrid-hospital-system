953ba75e2bda87c292fcc6633279f82056e24b56
Park gyeongsu <exodusean@gmail.com>
2026-05-13 12:41:20 +0900

    0513 #6 wazuh terraform import-4

--- terraform/aws/wazuh/provider.tf
+++ terraform/aws/wazuh/provider.tf
@@ -1,18 +1,3 @@
-terraform {
-  cloud {
-    organization = "k2p"
-    workspaces {
-      name = "TC-VPN"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "ap-south-2"
 
