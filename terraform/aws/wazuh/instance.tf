af802677784b63374b5234ee2b59dc7ba62edf21
kimkanghwan <kkhwan0822@gmail.com>
2026-05-19 10:36:02 +0900

    보안그룹 1514, 1515 온프레미스 대역 추가,이름 통일

--- terraform/aws/wazuh/instance.tf
+++ terraform/aws/wazuh/instance.tf
@@ -1,7 +1,7 @@
 #instance.tf
 
 # IAM Role
-resource "aws_iam_role" "wazuh_ssm_role" {
+resource "aws_iam_role" "aws-wazuh-ssm-role" {
   name = "aws-wazuh-ssm-role"
 
   assume_role_policy = jsonencode({
@@ -17,15 +17,15 @@ resource "aws_iam_role" "wazuh_ssm_role" {
 }
 
 # SSM 권한
-resource "aws_iam_role_policy_attachment" "wazuh_ssm" {
-  role       = aws_iam_role.wazuh_ssm_role.name
+resource "aws_iam_role_policy_attachment" "aws-wazuh-ssm" {
+  role       = aws_iam_role.aws-wazuh-ssm-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 # S3 권한 (Ansible SSM connection용)
-resource "aws_iam_role_policy" "wazuh_s3" {
-  name = "wazuh-s3-ssm"
-  role = aws_iam_role.wazuh_ssm_role.id
+resource "aws_iam_role_policy" "aws-wazuh-s3" {
+  name = "aws-wazuh-s3-ssm"
+  role = aws_iam_role.aws-wazuh-ssm-role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -47,9 +47,9 @@ resource "aws_iam_role_policy" "wazuh_s3" {
 }
 
 # EC2에 IAM Role 연결하는 다리
-resource "aws_iam_instance_profile" "wazuh_profile" {
+resource "aws_iam_instance_profile" "aws-wazuh-profile" {
   name = "aws-wazuh-instance-profile"
-  role = aws_iam_role.wazuh_ssm_role.name
+  role = aws_iam_role.aws-wazuh-ssm-role.name
 }
 
 resource "aws_key_pair" "aws-wazuh-key" {
@@ -68,7 +68,7 @@ resource "aws_instance" "aws-wazuh-01" {
   subnet_id              = data.aws_subnet.aws-app-sub-2a.id
   vpc_security_group_ids = [aws_security_group.aws-wazuh-sg.id]
   key_name               = aws_key_pair.aws-wazuh-key.key_name
-  iam_instance_profile   = aws_iam_instance_profile.wazuh_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.aws-wazuh-profile.name
 
   root_block_device {
     volume_size = 50
@@ -82,7 +82,7 @@ resource "aws_instance" "aws-wazuh-01" {
 }
 
 # hosts.ini 자동 생성
-resource "aws_s3_object" "ansible_hosts" {
+resource "aws_s3_object" "aws-ansible-hosts" {
   bucket  = "wazuh-ansible-ssm"
   key = "wazuh1/hosts.ini"
   content = <<-EOT
--- terraform/aws/wazuh/lambda.tf
+++ terraform/aws/wazuh/lambda.tf
@@ -1,15 +1,15 @@
 # wazuh/lambda.tf
 
 # Lambda 코드 zip 패키징
-data "archive_file" "slack_notify" {
+data "archive_file" "aws-wazuh-slack-notify" {
   type        = "zip"
   source_file = "${path.module}/lambda/slack_notify.py"
   output_path = "${path.module}/lambda/slack_notify.zip"
 }
 
 # IAM Role
-resource "aws_iam_role" "lambda_slack_notify" {
-  name = "lambda-wazuh-slack-notify-role"
+resource "aws_iam_role" "aws-wazuh-slack-notify-role" {
+  name = "aws-wazuh-slack-notify-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -22,19 +22,19 @@ resource "aws_iam_role" "lambda_slack_notify" {
 }
 
 # CloudWatch Logs 권한
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  role       = aws_iam_role.lambda_slack_notify.name
+resource "aws_iam_role_policy_attachment" "aws-wazuh-lambda-basic" {
+  role       = aws_iam_role.aws-wazuh-slack-notify-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 # Lambda 함수
-resource "aws_lambda_function" "slack_notify" {
-  function_name    = "wazuh-slack-notify"
-  role             = aws_iam_role.lambda_slack_notify.arn
+resource "aws_lambda_function" "aws-wazuh-slack-notify" {
+  function_name    = "aws-wazuh-slack-notify"
+  role             = aws_iam_role.aws-wazuh-slack-notify-role.arn
   handler          = "slack_notify.lambda_handler"
   runtime          = "python3.12"
-  filename         = data.archive_file.slack_notify.output_path
-  source_code_hash = data.archive_file.slack_notify.output_base64sha256
+  filename         = data.archive_file.aws-wazuh-slack-notify.output_path
+  source_code_hash = data.archive_file.aws-wazuh-slack-notify.output_base64sha256
 
   environment {
     variables = {
@@ -43,23 +43,23 @@ resource "aws_lambda_function" "slack_notify" {
   }
 
   tags = {
-    Name  = "wazuh-slack-notify"
+    Name  = "aws-wazuh-slack-notify"
     Owner = "st2"
   }
 }
 
 # SNS → Lambda 권한 (wazuh-01)
-resource "aws_lambda_permission" "sns_wazuh_01" {
+resource "aws_lambda_permission" "aws-wazuh-sns-01" {
   statement_id  = "AllowSNSWazuh01"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.slack_notify.function_name
+  function_name = aws_lambda_function.aws-wazuh-slack-notify.function_name
   principal     = "sns.amazonaws.com"
   source_arn    = aws_sns_topic.aws-wazuh-alerts-01.arn
 }
 
 # SNS 구독 (wazuh-01)
-resource "aws_sns_topic_subscription" "wazuh_01_to_lambda" {
+resource "aws_sns_topic_subscription" "aws-wazuh-01-to-lambda" {
   topic_arn = aws_sns_topic.aws-wazuh-alerts-01.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.slack_notify.arn
+  endpoint  = aws_lambda_function.aws-wazuh-slack-notify.arn
 }
