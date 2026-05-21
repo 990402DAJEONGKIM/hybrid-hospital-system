72ec37de28c51bbc46f1433b63eee2852ecf56fe
kimkanghwan <kkhwan0822@gmail.com>
2026-05-21 18:02:16 +0900

    ami 고정

--- terraform/aws/wazuh2/data.tf
+++ terraform/aws/wazuh2/data.tf
@@ -5,18 +5,7 @@ data "aws_vpc" "aws-vpc-01" {
 data "aws_subnet" "aws-app-sub-2b" {
   tags = { Name = "aws-app-sub-2b" }
 }
-data "aws_ami" "ubuntu_22_04" {
-  most_recent = true
-  owners      = ["099720109477"]
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+
 
 data "terraform_remote_state" "wazuh" {
   backend = "remote"
--- terraform/aws/wazuh2/instance.tf
+++ terraform/aws/wazuh2/instance.tf
@@ -9,7 +9,7 @@ data "aws_key_pair" "aws-wazuh-key" {
 
 # EC2
 resource "aws_instance" "aws-wazuh-02" {
-  ami                    = data.aws_ami.ubuntu_22_04.id
+  ami                    = "ami-0eab39170eb2844c5"
   instance_type          = "t3.large"
   subnet_id              = data.aws_subnet.aws-app-sub-2b.id
   vpc_security_group_ids = [data.aws_security_group.aws-wazuh-sg.id]
