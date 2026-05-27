33b5d2216453dba64f31d9f838bbf44709a295ee
kimkanghwan <kkhwan0822@gmail.com>
2026-05-27 10:42:44 +0900

    wazuh-01 output sensitive 추가

--- terraform/aws/wazuh/data.tf
+++ terraform/aws/wazuh/data.tf
@@ -34,13 +34,17 @@ data "terraform_remote_state" "kms" {
 }
 output "wazuh_private_ip" {
   value = aws_instance.aws-wazuh-01.private_ip
+  sensitive = true
 }
 
 output "wazuh_instance_id" {
   value = aws_instance.aws-wazuh-01.id
+  sensitive = true
 }
 # wazuh_private_ip는 이미 있음
 
 output "wazuh_instance_arn" {
   value = aws_instance.aws-wazuh-01.arn
+  sensitive = true
 }
+
