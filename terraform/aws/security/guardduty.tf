0c498c2c5a18ab4ab422bc91d725cb9c292ce252
kimkanghwan <kkhwan0822@gmail.com>
2026-05-30 00:30:14 +0900

    security tf 파일 생성

--- terraform/aws/security/guardduty.tf
+++ terraform/aws/security/guardduty.tf
@@ -10,34 +10,7 @@ resource "aws_guardduty_detector" "aws-gd" {
   }
 }
 
-# S3 버킷 정책 (GuardDuty가 쓸 수 있도록)
-resource "aws_s3_bucket_policy" "aws-s3-policy-gd" {
-  bucket = "aws-k2p-storage-01"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowGuardDutygetBucketLocation"
-        Effect = "Allow"
-        Principal = {
-          Service = "guardduty.amazonaws.com"
-        }
-        Action   = "s3:GetBucketLocation"
-        Resource = "arn:aws:s3:::aws-k2p-storage-01"
-      },
-      {
-        Sid    = "AllowGuardDutyPutObject"
-        Effect = "Allow"
-        Principal = {
-          Service = "guardduty.amazonaws.com"
-        }
-        Action   = "s3:PutObject"
-        Resource = "arn:aws:s3:::aws-k2p-storage-01/guardduty/*"
-      }
-    ]
-  })
-}
 
 # GuardDuty → S3 직접 내보내기
 resource "aws_guardduty_publishing_destination" "aws-gd-s3" {
@@ -45,6 +18,24 @@ resource "aws_guardduty_publishing_destination" "aws-gd-s3" {
   destination_arn = "arn:aws:s3:::aws-k2p-storage-01"
   kms_key_arn     =  data.terraform_remote_state.kms.outputs.s3_kms_key_arn
 
-  depends_on = [aws_s3_bucket_policy.aws-s3-policy-gd]
+
 }
 
+
+resource "aws_cloudwatch_event_rule" "aws-gd-high" {
+  name        = "aws-guardduty-high-severity"
+  description = "GuardDuty HIGH 이상 finding 실시간 알람"
+
+  event_pattern = jsonencode({
+    source      = ["aws.guardduty"]
+    detail-type = ["GuardDuty Finding"]
+    detail = {
+      severity = [{ numeric = [">=", 7] }]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "aws-gd-sns" {
+  rule = aws_cloudwatch_event_rule.aws-gd-high.name
+  arn  = "arn:aws:sns:ap-south-2:476293896981:aws-wazuh-cw-alerts-01"
+}
--- terraform/aws/security/vpc_flowlogs.tf
+++ terraform/aws/security/vpc_flowlogs.tf
@@ -1,40 +1,6 @@
 resource "aws_flow_log" "aws-vpc-flowlog" {
   vpc_id          = data.aws_vpc.aws-vpc-01.id
   traffic_type    = "ALL"
-  iam_role_arn    = aws_iam_role.aws-iam-role-flowlog.arn
-  log_destination = aws_cloudwatch_log_group.aws-cloudwatch-vpcflow.arn
-}
-
-resource "aws_cloudwatch_log_group" "aws-cloudwatch-vpcflow" {
-  name              = "/aws/vpc/flowlogs"
-  retention_in_days = 90
-}
-
-resource "aws_iam_role" "aws-iam-role-flowlog" {
-  name = "aws-iam-role-flowlog"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "vpc-flow-logs.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "aws-iam-policy-flowlog" {
-  name = "aws-iam-policy-flowlog"
-  role = aws_iam_role.aws-iam-role-flowlog.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "logs:CreateLogGroup", "logs:CreateLogStream",
-        "logs:PutLogEvents", "logs:DescribeLogGroups",
-        "logs:DescribeLogStreams"
-      ]
-      Resource = "*"
-    }]
-  })
+  log_destination_type     = "s3"
+  log_destination = "arn:aws:s3:::aws-k2p-storage-01/flowlogs/"
 }
