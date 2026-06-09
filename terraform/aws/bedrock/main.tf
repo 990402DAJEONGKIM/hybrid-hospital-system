194d654235aefe2cbe7253ffa1d96f7d7aef6dd2
dj <naver.com>
2026-06-09 21:44:38 +0000

    lambda layer 추가

--- terraform/aws/bedrock/main.tf
+++ terraform/aws/bedrock/main.tf
@@ -99,6 +99,7 @@ resource "aws_lambda_function" "gcp_billing_collector" {
   memory_size      = 256
   filename         = data.archive_file.gcp_billing_collector.output_path
   source_code_hash = data.archive_file.gcp_billing_collector.output_base64sha256
+  layers           = ["arn:aws:lambda:ap-south-2:476293896981:layer:gcp-python-deps:1"]
 
   environment {
     variables = {
